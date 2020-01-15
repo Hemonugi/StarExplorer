43494287a2e55a2ab04520921b79b249292de242
Hemonugi Meno <hemonugi@gmail.com>
2020-01-15 23:15:55 +1100

    fix set background

--- main.lua
+++ main.lua
@@ -43,15 +43,16 @@ local sheetOptions =
 }
 
 local objectSheet = graphics.newImageSheet('gameObjects.png', sheetOptions)
-local ship
-
-local background = display.newImageRect( backGroup, "background.png", 800, 1400 )
-background.x = display.contentCenterX
-background.y = display.contentCenterY
 
 local backGroup = display.newGroup()
 local mainGroup = display.newGroup()
 local uiGroup = display.newGroup()
+
+local ship
+
+local background = display.newImageRect(backGroup, 'background.png', 800, 1400 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
 
 ship = display.newImageRect(mainGroup, objectSheet, 4, 98, 79)
 ship.x = display.contentCenterX
